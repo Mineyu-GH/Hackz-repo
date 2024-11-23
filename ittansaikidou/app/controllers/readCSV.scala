d371ccd1b23d251d0c8b88d8a39ac72bd71e46ce
CaramelMio <130810262+mono-repository@users.noreply.github.com>
2024-11-23 20:39:11 +0900

    csvファイルを読み込み

--- ittansaikidou/app/controllers/readCSV.scala
+++ ittansaikidou/app/controllers/readCSV.scala
@@ -4,18 +4,32 @@ import javax.inject._
 import play.api.mvc._
 import play.api.Environment
 import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
+
+
+
 
 @Singleton
-class readCSV @Inject()(val controllerComponents: ControllerComponents, env: Environment) extends BaseController {
+class readCSV @Inject()(
+                         val controllerComponents: ControllerComponents,
+                         env: Environment
+                       ) extends BaseController {
 
   def printCSV = Action {
-    val csvFile = env.getFile("public/data/data.csv")
+    val csvFile = env.getFile("public/db/data.csv")
 
     try {
-      val csvContent = Source.fromFile(csvFile).getLines().mkString("\n")
-      //CSVコンテントをデータベースへ書き込む
+      // CSVファイルの内容を取得
+      val csvContent = Source.fromFile(csvFile).getLines()
 
-      Ok(csvContent)
+      // カンマで分割してSetに格納
+      val wordSet: Set[String] = csvContent.flatMap(_.split(",").map(_.trim)).toSet
+
+      // デバッグ用の出力
+      println(wordSet)
+
+      // レスポンスとして返す（任意）
+      Ok(wordSet.mkString("\n"))
     } catch {
       case e: Exception => InternalServerError(s"Error reading CSV file: ${e.getMessage}")
     }
--- ittansaikidou/build.sbt
+++ ittansaikidou/build.sbt
@@ -6,6 +6,12 @@ organization := "com.example"
 version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
+  .enablePlugins(DockerPlugin)
+  .settings(
+      Universal / javaOptions ++= Seq("-Dpidfile.path=/dev/null"),
+//      dockerBaseImage := "eclipse-temurin:17",
+      dockerExposedPorts ++= Seq(9000)
+  )
 
 scalaVersion := "2.13.15"
 
@@ -13,5 +19,5 @@ libraryDependencies ++= Seq(
     guice,
     javaJdbc,
     "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1" % Test,
-    "mysql" % "mysql-connector-java" % "8.0.33"
+    "mysql" % "mysql-connector-java" % "8.0.33",
 )
